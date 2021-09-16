bc1c0a30d82ebf25c7557249ab1f258d89efc35c
Artur Shellunts <shellunts.artur@gmail.com>
2021-09-16 21:38:23 +0200

    Use requite in the test

--- src/RunCommand_test.go
+++ src/RunCommand_test.go
@@ -3,20 +3,20 @@ package main
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRunCommand(t *testing.T) {
-	assert := assert.New(t)
+	require := require.New(t)
 
 	dataPipe, err := RunCommand("test_program.exe")
-	assert.Nil(err)
+	require.NoError(err)
 
 	program_output := make([]byte, 100)
 	n, err := dataPipe.Read(program_output)
-	assert.Nil(err)
-	assert.Equal(6, n)
+	require.NoError(err)
+	require.Equal(6, n)
 
 	expected := []byte{72, 101, 108, 108, 111, 10}
-	assert.Equal(expected, program_output[0:n])
+	require.Equal(expected, program_output[0:n])
 }
